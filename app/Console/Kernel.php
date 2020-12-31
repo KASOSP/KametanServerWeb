e20971d870579ebe0d369309773e29014d5ebc97
kametan0730MC <kametan0730@gmail.com>
2021-01-01 02:15:50 +0900

    不要なファイルを削除

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,6 +26,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command('command:every-minutes-batch')->everyMinute();
+
     }
 
     /**
